c2926ed603ae816848292e92b86e01b2efbf3015
domAK45 <dombosnjak@gmail.com>
2023-04-27 10:52:57 +0200

    crete constants module

---
+++ widgets/constants.py
@@ -0,0 +1,18 @@
+"""General constants module for ranah app
+
+If the app grows larger, it'd probably make sense to split
+this module into several modules within a (sub)package
+"""
+
+text_constants = {
+        'topic_lbl': 'Nutritivne vrijednosti na 100 grama',
+        'calory_lbl': 'Kalorije',
+        'fat_lbl': 'Masti',
+        'sat_fat_lbl': 'Zasićene masti',
+        'carb_lbl': 'Ugljikohidrati',
+        'sugar_lbl': 'Šećeri',
+        'protein_lbl': 'Bjelančevine',
+        'fiber_lbl': 'Vlakna',
+        'food_name_lbl': 'Ime',
+        'create_btn': 'Kreiraj',
+    }
